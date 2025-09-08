176a374a055bd6fee981963777e15346e7f86419
On3l22 <onel.magallon@utp.ac.pa>
2025-09-08 11:35:13 -0500

    archivos de programas para calcular el area de un rectangulo y un circulo

---
+++ CaculoArea/CaculoArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//Calculo del area de un triangulo.
+class Program //Clase
+{
+    static void Main(string[] args)//Main
+    {
+        //----------- Area de un rectangulo ---------------
+        Console.WriteLine("-- Área del Triangulo --");
+        Console.Write("Ingresa la base: ");
+        //Guarda la base ingresada
+        var baseRec = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingresa la altura: ");
+        //Guarda la altura ingresada
+        var alturaRec = Convert.ToDouble(Console.ReadLine());
+
+        var areaRec = baseRec * alturaRec; //calculo
+        Console.WriteLine($"El área del triangulo es: {areaRec}");
+    }
+}
+
---
+++ CalculoAreaCirculo/CalculoAreaCirculo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//Calculo del area de un circulo.
+class Program //Clase
+{
+    static void Main(string[] args)//Main
+    {
+        //---------- Area de un circulo -------------------
+        Console.WriteLine("\n-- Área del Circulo --");
+        Console.Write("Ingresa el radio: ");
+
+        //Guarda el radio
+        var radio = Convert.ToDouble(Console.ReadLine());
+
+        var areaCirc = Math.PI * Math.Pow(radio, 2);//calculo
+        var formateo = areaCirc.ToString("0.00");
+        Console.WriteLine($"El área del circulo es: {formateo}");
+    }
+}
