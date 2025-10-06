0a43ce858e163b920a8565a712a6f3497c71d5a3
On3l22 <onel.magallon@utp.ac.pa>
2025-10-06 08:31:08 -0500

    Uso de LINQ, Videos del 1 al 16

---
+++ UsoLINQ/UsoLINQ/LinqQueries.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoLINQ
+{
+    //================= CLASE DE CONSULTAS ==================
+    internal class LinqQueries
+    {
+        private List<Book> librosCollection = new List<Book>();//Lista de libros
+        public LinqQueries() //Constructor
+        {
+            using (StreamReader reader = new StreamReader("books.json"))//lee cada linea del archivo json
+            {
+                string json = reader.ReadToEnd();
+                this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            }
+        }
+
+        //Metodo -> Extrar los datos de todos los libros
+        public IEnumerable<Book> TodaLaCollection()
+        {
+            return librosCollection;
+        }
+
+        //Metodo -> Extrar libros publicados despues del año 2000
+        public IEnumerable<Book> booskBeforeOf2000()
+        {
+            return librosCollection.Where(p => p.PublishedDate.Year > 2000);
+        }
+
+        //Metodo -> Extraer libros del 2000 usando Query expresion
+        public IEnumerable<Book> booksBeforeOf200QryExp()
+        {
+            return from l in librosCollection where l.PublishedDate.Year > 2000 select l;
+        }
+
+        //Metodo -> Extraer libros con 250pag. y Titulo contenga Action.
+        public IEnumerable<Book> books250pagANDtileAction()
+        {
+            //return librosCollection.Where(p => p.PageCount > 250 && p.Title.Contains("in Action"));
+            return from l in librosCollection where l.PageCount > 250 && l.Title.Contains("in Action") select l;
+        }
+
+        //Metodo -> Uso de operador ALL, Verificar si todos los libros tienen status
+        public bool UseOfOperatorALL()
+        {
+            return librosCollection.All(p => p.Status != string.Empty);
+        }
+
+        //Metodo -> Uso de ANY para verificar si algun libro fue publicado en el 2005
+        public bool UseOfOperatorANY()
+        {
+            return librosCollection.Any(p => p.PublishedDate.Year == 2005);
+        }
+
+        //Metodo -> Uso de Contains, Retornar los elementos con categoria Python
+        public IEnumerable<Book> booksCatPython() 
+        {
+            return librosCollection.Where(p => p.Categories.Contains("Python"));
+        }
+
+        //Metodo -> Uso de OrderBy, retornar los elementos que sean de la categoria java ordenados por nombre
+        public IEnumerable<Book> booksCatJavaOrderByName()
+        {
+            return librosCollection.Where(p => p.Categories.Contains("Java")).OrderBy(p => p.Title);
+        }
+
+        //Metod -> Uso de OrderByDescending, Retornar los libros que tengan mas de 450 paginas
+        //Ordenados por numero de paginas de forma descendente
+        public IEnumerable<Book> booksMore450PagOrderByDes()
+        {
+            return librosCollection.Where(p => p.PageCount > 450).OrderByDescending(p => p.PageCount);
+        }
+
+        //Metodo -> Uso de Take para optener los tres ultimos libros ordenados por fecha
+        public IEnumerable<Book> threeBooksJavaOrderByDate()
+        {
+            return librosCollection
+                .Where(p => p.Categories.Contains("Java"))
+                .OrderBy(p => p.PublishedDate)
+                .TakeLast(3);
+        }
+
+        //Metodo -> Uso de Skip para obtener el tercer y cuarto libro con mas de 400 paginas
+        public IEnumerable<Book> thirdAndFourthBooksMore400pages()
+        {
+            return librosCollection
+                .Where(p => p.PageCount > 400)
+                .Take(4)
+                .Skip(2);
+        }
+        
+        //Metodo -> Uso de Seleccion dinamica
+        public IEnumerable<Book> ThreeBooksInCollection()
+        {
+            return librosCollection
+                .Take(3)
+                .Select(p => new Book() { Title = p.Title, PageCount = p.PageCount });//Se crea una nueva coleccion solo con dos datos
+        }
+    }
+
+}
---
+++ UsoLINQ/UsoLINQ/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using UsoLINQ;
+
+//================== USO BASICO ===================
+//Uso sencillo de linq para hacer consulta
+//Lista de frutas
+var frutas = new string[] { "Sandia", "Fresa", "Mango", "Mango de azucar", "Mango verde" };
+
+//Lista que guarda las frutas encontradas
+var mangoList = frutas.Where(p=> p.StartsWith("Mango")).ToList();
+mangoList.ForEach(p=> Console.WriteLine(p));
+
+
+
+
+
+//============== USO AVANZADO  ====================
+LinqQueries queries = new LinqQueries();//Nueva instancia de la clase consulta
+
+//Metodo -> Imprimir datos en consola
+void printfValues(IEnumerable<Book> listBooks)
+{
+    //Formato de impresion
+    Console.WriteLine("{0, -60} {1, 15} {2, 15}\n ", "Titulo", "N. Paginas", "Fecha de publicacion");
+    foreach(var item in listBooks)
+    {
+        Console.WriteLine("{0, -60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
+
+//Imprimir toda la coleccion
+Console.WriteLine("------- TODOS LOS LIBROS");
+printfValues(queries.TodaLaCollection());//Impresion 
+Console.WriteLine("\n\n\n\n\n");
+
+//Imprimir libros publicados en el año 2000
+Console.WriteLine("------- LIBROS PUBLICADOS DESPUES DEL 2000");
+printfValues(queries.booskBeforeOf2000());//Impresion
+Console.WriteLine("\n\n\n\n\n");
+
+//Imprimir libros del 2000 usando Query expresion
+Console.WriteLine("------- USO DE QUERY EXPRESION");
+printfValues(queries.booksBeforeOf200QryExp());
+Console.WriteLine("\n\n\n\n\n");
+
+//Imprimir libros con 250 paginas y con el titulo Action
+Console.WriteLine("------- LIBROS CON 250 PAGINAS Y QUE CONTENGA ACTION");
+printfValues(queries.books250pagANDtileAction());
+Console.WriteLine("\n\n\n\n\n");
+
+//Imprimir Verificar si los libros tiene status
+Console.WriteLine("------- USO DE ALL");
+Console.WriteLine("Todos los libros tiene status?: "+ queries.UseOfOperatorALL());
+Console.WriteLine("\n\n\n\n\n");
+
+//Imprimir si un libro fue publicado en el 2005
+Console.WriteLine("------- USO DE ANY");
+Console.WriteLine("Algun libro fue publicado en el 2005?: " + queries.UseOfOperatorANY());
+Console.WriteLine("\n\n\n\n\n");
+
+//Imprimir los libros con categoria Python
+Console.WriteLine("------- USO DE CONTAINS");
+printfValues(queries.booksCatPython());
+Console.WriteLine("\n\n\n\n\n");
+
+//Imprimir los libros con categoria Java y otdenarlo por nombre usando OrderBy
+Console.WriteLine("------- USO DE ORDER BY");
+printfValues(queries.booksCatJavaOrderByName());
+Console.WriteLine("\n\n\n\n\n");
+
+//Imprimir los libros que tenga mas de 450 paginas ordenados descendentemente
+Console.WriteLine("------- USO DE ORDER BY DECENDING");
+printfValues(queries.booksMore450PagOrderByDes());
+Console.WriteLine("\n\n\n\n\n");
+
+//Imprimir los tres ultimos libros
+Console.WriteLine("------- USO DE TAKE");
+printfValues(queries.threeBooksJavaOrderByDate());
+Console.WriteLine("\n\n\n\n\n");
+
+//Imprimir el tercer y cuarto libro con mas de 400 paginas
+Console.WriteLine("------- USO DE SKIP");
+printfValues(queries.thirdAndFourthBooksMore400pages());
+Console.WriteLine("\n\n\n\n\n");
+
+//Imprimir el tercer y cuarto libro con mas de 400 paginas
+Console.WriteLine("------- USO DE SELECCION DINAMICA");
+printfValues(queries.ThreeBooksInCollection());
+Console.WriteLine("\n\n\n\n\n");
