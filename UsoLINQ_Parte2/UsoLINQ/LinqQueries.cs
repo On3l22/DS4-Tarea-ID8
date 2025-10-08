a0b8f144601583ce13a9ebdd6864f14bd34c8fba
On3l22 <onel.magallon@utp.ac.pa>
2025-10-07 20:44:28 -0500

    Segunda parte de los videos 17 al 26 Uso de LINQ

---
+++ UsoLINQ_Parte2/UsoLINQ/LinqQueries.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoLINQ
+{
+    //================= CLASE DE CONSULTAS ==================
+    internal class LinqQueries
+    {
+        private List<Book> librosCollection = new List<Book>();//Lista de libros
+        public LinqQueries() //Constructor
+        {
+            using (StreamReader reader = new StreamReader("books.json"))//lee cada linea del archivo json
+            {
+                string json = reader.ReadToEnd();
+                this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            }
+        }
+
+        //#################################################################
+        //SEGUNDA PARTE DE LOS RETOS
+
+        //====== METODO Uso de COUNT ==================
+        //Uso de count y longCount para mostrar la cantidad de libros mayores o iguales a 200 paginas y menor o igual a 500
+        public int CantBooks200And500Pages_int()
+        {
+            return librosCollection.Where(p => p.PageCount >= 200 && p.PageCount <= 500).Count();
+        }
+
+        //====== Uso de LongCount =====================
+        public long cantBooks200And500Pages_long()
+        {
+            return librosCollection.LongCount(p => p.PageCount >= 200 && p.PageCount <= 500);
+        }
+
+        //====== METODO uso de Min ===================
+        //Uso de Min, retornar la fecha menor de libros publicados
+        public DateTime MenorDate()//Min
+        {
+            return librosCollection.Min(p => p.PublishedDate);
+        }
+
+        //====== Uso de Max =========================
+        public int MaxPagBooks()//Max
+        {
+            return librosCollection.Max(p => p.PageCount);
+        }
+
+        //======= Método ============================
+        // USo de MinBy
+        public Book booksWhitMenorPages()//MinBy
+        {
+            // MinBy puede devolver null si la colección está vacía, así que se debe manejar ese caso.
+            return librosCollection
+                .Where(p => p.PageCount > 0)
+                .MinBy(p => p.PageCount) 
+                ?? new Book();
+        }
+
+        //====== Uso de MaxBy ========================
+        public Book bookDatePublichResent()//MaxBy
+        {
+            return librosCollection
+                .MaxBy(p => p.PublishedDate) 
+                ?? new Book();
+        }
+
+        //====== Metodo Que usa el Operador SUM ======================
+        public int sumThePagesBooks() {
+            return librosCollection
+                .Where(p => p.PageCount >= 0 && p.PageCount <= 500)
+                .Sum(p => p.PageCount);
+        }
+
+        //====== Metodo Que usa Aggregate ============================
+        public string titleBooksBefore2015()
+        {
+            return librosCollection
+                .Where(p => p.PublishedDate.Year > 2015)
+                .Aggregate("", (titleBooks, next) =>
+                {
+                    if (titleBooks != string.Empty)
+                        titleBooks += "\n" + next.Title;
+                    else
+                        titleBooks += next.Title;
+                    return titleBooks;
+                });
+        }
+
+        //====== Metodo que usa Average =============================
+        //Retorna el pormedio de caracteres que tienen los titulos de la coleccion
+        public double PromedianCharactBoosk()
+        {
+            return librosCollection.Average(p => p.Title.Length);
+        }
+
+        //---------------------------
+        // AGRUPACION
+        //---------------------------
+        
+        //====== Metodo que usa GroupBy
+        //Libros publicados a partir del 2000 agrupados por anio
+        public IEnumerable<IGrouping<int, Book>> booksAfterThe2000GroupForYear()
+        {
+            return librosCollection
+                .Where(p => p.PublishedDate.Year >= 2000)
+                .GroupBy(p => p.PublishedDate.Year);
+        }
+
+        //====== Metodo que usa Lookup
+        /*Retorna un diccionario usando lookup que permite consultar los libros
+          de acuerdo a la letra con la que inicia le titulo del libro*/
+        public ILookup<char, Book> diccionaryTheBooksByLetter()
+        {
+            return librosCollection
+                .ToLookup(p => p.Title[0], p => p);
+        }
+
+        //Metodo para usar el operador JOIN
+        /*
+         Obtener una coleccion que tenga todos los libros con mas de 500 paginas y
+         que contenga los libros publicados despues del 2005 utilizando la clausula
+         join, retornar los libros que esten en ambas colecciones
+         */
+        public IEnumerable<Book> UseJoin()
+        {
+            var booksAfter2005 = librosCollection.Where(p => p.PublishedDate.Year > 2005);
+            var booksWhiteMore500Pages = librosCollection.Where(p => p.PageCount > 500);
+
+            return booksAfter2005.Join(booksWhiteMore500Pages, p => p.Title, x => x.Title, (p, x) => p);
+        }
+    }
+}
---
+++ UsoLINQ_Parte2/UsoLINQ/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using UsoLINQ;
+
+//============== USO AVANZADO  ====================
+LinqQueries queries = new LinqQueries();//Nueva instancia de la clase consulta
+
+//Metodo -> Imprimir datos en consola
+void printfValues(IEnumerable<Book> listBooks)
+{
+    //Formato de impresion
+    Console.WriteLine("{0, -60} {1, 15} {2, 15}\n ", "Titulo", "N. Paginas", "Fecha de publicacion");
+    foreach(var item in listBooks)
+    {
+        Console.WriteLine("{0, -60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
+
+//#################################################
+Console.WriteLine("###### SEGUNDA PARTE\n1.=============\nCount: " + queries.CantBooks200And500Pages_int());
+Console.WriteLine("LongCount: " + queries.cantBooks200And500Pages_long() + "\n\n");
+
+//Uso de MIN
+Console.WriteLine($"2.===========\nDe la fecha menor de publicacion: {queries.MenorDate()}");
+//Uso Max
+Console.WriteLine($"Numero mayores de paginas: {queries.MaxPagBooks()}");
+//Uso de MinBy
+var bookMenorPages = queries.booksWhitMenorPages();
+Console.WriteLine($"3.===========\n{bookMenorPages.Title} - {bookMenorPages.PageCount}\n\n");
+//Uso de MaxBy
+var bookDateRecent = queries.bookDatePublichResent();
+Console.WriteLine($"4.===========\n{bookDateRecent.Title} - {bookDateRecent.PublishedDate.ToShortDateString()}\n\n");
+
+//Uso de SUM
+Console.WriteLine($"5.===========" +
+    $"\nLa suma de todos los libros de entre 0 y 500 paginas son: {queries.sumThePagesBooks()}");
+
+//Uso de Aggregate
+Console.WriteLine($"\n6.===========" +
+    $"\nLibros Despues del 2015:" +
+    $"\n{queries.titleBooksBefore2015()}");
+
+//Uso de Average
+Console.WriteLine($"\n7.===========" +
+    $"\nPromeido de caracteres de los titulos: {queries.PromedianCharactBoosk()}" +
+    $"\n");
+
+//Uso de GroupBy
+//Funcion para poder imprimir la lista de libros
+void printfGroup (IEnumerable<IGrouping<int, Book>> ListadoDeLibros)
+{
+    foreach (var group in ListadoDeLibros)
+    {
+        Console.WriteLine("");
+        Console.WriteLine($"Grupo: {group.Key} -------------------------------");
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha de publicacion");
+        foreach (var item in group)
+        {
+            Console.WriteLine("{0, -60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.Date.ToShortDateString());
+        }
+    }
+}
+
+Console.WriteLine("\n8.============");
+printfGroup(queries.booksAfterThe2000GroupForYear());
+
+//Uso de Lookup
+//Funcion para imprimir el diccionario
+void printfDiccionary (ILookup<char, Book> listaDeLibros, char letra)
+{
+    Console.WriteLine("{0, -60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha de publicacion");
+    foreach (var item in listaDeLibros[letra])
+    {
+        Console.WriteLine("{0, -60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.Date.ToShortDateString());
+    }
+}
+
+Console.WriteLine("\n9.===========");
+printfDiccionary(queries.diccionaryTheBooksByLetter(), 'E');
+
+//Uso de JOIN
+Console.WriteLine("\n10.==========");
+printfValues(queries.UseJoin());
